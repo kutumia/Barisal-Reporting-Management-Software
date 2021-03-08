488f25a887d7458cd42791c904a8d50457743c3c
njayman <najishm828282@gmail.com>
2021-03-08 22:15:56 +0600

    added redis, sequalize loging disabled, env established

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { DBHOST, DBUSER, DBPASSWORD, DB } = process.env;
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "barisal",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: DBHOST,
+  USER: DBUSER,
+  PASSWORD: DBPASSWORD,
+  DB: DB,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
